e1d5ca671973af69b009406bf126e8c66b44c000
Manoel Duran <manoel.duran@hotmail.com>
2023-03-18 17:50:15 -0300

    handler exception

---
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -0,0 +1,19 @@
+import { ArgumentsHost, Catch } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const messsage = exception.message;
+
+    switch (exception.code) {
+      case 'P2002':
+        break;
+      default:
+        super.catch(exception, host);
+    }
+  }
+}
